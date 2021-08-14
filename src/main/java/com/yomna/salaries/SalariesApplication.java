bbd8ff97f9167959c08e492b44b0c4440a771587
Yomna Hesham <yomna.heshamyh@hotmail.com>
2021-08-14 11:18:44 +0200

    init project

---
+++ src/main/java/com/yomna/salaries/SalariesApplication.java
@@ -0,0 +1,15 @@
+package com.yomna.salaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalariesApplication.class, args);
+
+        System.out.println("Application Started !");
+    }
+
+}
---
+++ src/test/java/com/yomna/salaries/SalariesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yomna.salaries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
