94d7c88617285a03b3e13908df5170bc1eeee0b6
Yomna Hesham <yomna.heshamyh@hotmail.com>
2021-08-21 08:21:05 +0200

    rename class

--- src/main/java/com/yomna/salaries/service/IndividualService.java
+++ src/main/java/com/yomna/salaries/service/IndividualService.java
@@ -5,6 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomerService {
+public class IndividualService {
     @Autowired private IndividualRepository repository;
 }
